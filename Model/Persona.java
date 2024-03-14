ba2b62b4a4c699b46bd4c00a677c45b01431e40b
Grumash <162335328+Grumash@users.noreply.github.com>
2024-03-14 16:36:08 +0100

    Persona

---
+++ Model/Persona.java
@@ -0,0 +1,113 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    public string dni;
+
+    /**
+     * 
+     */
+    public string nombre;
+
+    /**
+     * 
+     */
+    public string apellidos;
+
+    /**
+     * 
+     */
+    public int edad;
+
+    /**
+     * @param dni 
+     * @param nombre 
+     * @param apellidos 
+     * @param edad
+     */
+    public void persona(string dni, string nombre, string apellidos, int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * Modifica el el dni de la persona
+     * @param dni
+     */
+    public void setDni(string dni) {
+        // TODO implement here
+    }
+
+    /**
+     * debuelve el dni de la persona
+     * @return
+     */
+    public string getDni() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * Modifica el el nombre de la persona
+     * @param nombre
+     */
+    public void setNombre(string nombre) {
+        // TODO implement here
+    }
+
+    /**
+     * debuelve el nombrede la persona
+     * @return
+     */
+    public string getNombre() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * Modifica el el nombre de la persona
+     * @param apellido
+     */
+    public void setApellido(string apellido) {
+        // TODO implement here
+    }
+
+    /**
+     * debuelve el apellido de la persona
+     * @return
+     */
+    public string getApellido() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * Modifica el el nombre de la persona
+     * @param edad
+     */
+    public void setEdad(int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * debuelve el edad de la persona
+     * @return
+     */
+    public int getEdad() {
+        // TODO implement here
+        return 0;
+    }
+
+}
